42dab9358c94571c83b8fa608b6b24cc1293a57c
Brent Martin <bmart35@gmail.com>
2016-03-23 19:50:28 -0500

    Added new and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,18 +28,6 @@ class TaskController < ApplicationController
     render_template 'tasks/new.html.erb'
   end
 
-  def create #POST
-    last_task = App.tasks.max_by { |task| task.id }
-    new_id = last_task.id + 1
-
-    App.tasks.push(
-      Task.new(new_id, params["body"], false)
-    )
-    puts App.tasks.to_json
-
-    render({ message: "Successfully created!", id: new_id }.to_json)
-  end
-
   def update #PUT
     task = find_task_by_id
 
@@ -54,32 +42,3 @@ class TaskController < ApplicationController
       render_not_found
     end
   end
-
-
-  def destroy #DELETE
-    task = find_task_by_id
-
-    if task
-      App.task.delete(task)
-      render({ message: "Successfully Deleted Task" }.to_json)
-    else
-      render_not_found
-    end
-  end
-
-  private
-
-  def find_task_by_id
-    App.task.find { |t| t.id == params[:id].to_i }
-  end
-
-  def render_not_found
-    return_message = {
-      message: "Task not found!",
-      status: '404'
-    }.to_json
-
-    render return_message, status: "404 NOT FOUND"
-  end
-
-end
